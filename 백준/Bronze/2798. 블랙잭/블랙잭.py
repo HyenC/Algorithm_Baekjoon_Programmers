69541dfae974a7c5683bd4e16fe645615af75832
HyenC <38906574+HyenC@users.noreply.github.com>
2023-09-19 23:54:24 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,12 @@
+from itertools import combinations
+ 
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
-sum_list = []
-
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
-            if nums[i] + nums[j] + nums[k] <= M:
-                sum_list.append(nums[i] + nums[j] + nums[k])
-                
-print(sorted(sum_list)[-1])
+ 
+result = 0
+ 
+for i in combinations(nums, 3):
+    if result < sum(i) and sum(i) <= M:
+        result = sum(i)
+ 
+print(result)
