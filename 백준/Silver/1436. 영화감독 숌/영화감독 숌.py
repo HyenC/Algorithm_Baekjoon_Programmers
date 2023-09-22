f1b8d7b4f62b671aa195551e530a42d022e3ae48
HyenC <38906574+HyenC@users.noreply.github.com>
2023-09-23 02:16:43 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,13 +1,16 @@
 N = int(input())
+s, cnt = [666], 1
 
-num = 666
-cnt = 0
+while(len(s) < N):
+    m = cnt * 1000
+    if(cnt % 1000 == 666):
+        s += [m + i for i in range(1000)]
+    elif(cnt % 100 == 66):
+        s += [m + 600 + i for i in range(100)]
+    elif(cnt % 10 == 6):
+        s += [m + 660 + i for i in range(10)]
+    else:
+        s += [m + 666]
+    cnt += 1
 
-while True:
-    if '666' in str(num):
-        cnt += 1
-        if cnt == N:
-            break
-    num += 1
-
-print(num)
+print(s[N - 1])
