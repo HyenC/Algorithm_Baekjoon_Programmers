c6286c15b4a9c80e35871353cb2c83bcbb0f8bb2
HyenC <38906574+HyenC@users.noreply.github.com>
2023-11-10 00:39:08 +0900

    [level 1] Title: 모의고사, Time: 3.26 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,15 @@
+def solution(answers):
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0,0,0]
+    
+    for i, answer in enumerate(answers):
+    	if answer == s1[i % len(s1)]:
+        	score[0] += 1
+    	if answer == s2[i % len(s2)]:
+        	score[1] += 1
+    	if answer == s3[i % len(s3)]:
+        	score[2] += 1
+
+    return [i + 1 for i, s in enumerate(score) if s == max(score)]
