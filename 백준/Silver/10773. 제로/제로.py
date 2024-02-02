349e9567248bd49f6b61fb47aa7a44dc123b1b22
HyenC <38906574+HyenC@users.noreply.github.com>
2024-02-02 22:12:11 +0900

    [Silver IV] Title: 제로, Time: 88 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,11 +1,15 @@
-K = int(input())
+import sys
+
+K = int(sys.stdin.readline().strip())
 s = []
+total_sum = 0
 
 for _ in range(K):
-    n = int(input())
+    n = int(sys.stdin.readline().strip())
     if n == 0:
-        s.pop()
+        total_sum -= s.pop()
     else:
         s.append(n)
+        total_sum += n
         
-print(sum(s))
+print(total_sum)  # 리스트를 다시 순회할 필요 없이 결과 출력
