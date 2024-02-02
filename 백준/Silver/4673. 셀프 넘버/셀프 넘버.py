a376153157f318efeb214f75bfc9e965fe7b0abc
HyenC <38906574+HyenC@users.noreply.github.com>
2024-02-02 21:30:45 +0900

    [Silver V] Title: 셀프 넘버, Time: 48 ms, Memory: 32148 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,11 +1,11 @@
-Num = set(range(1, 10001))
-NonSelfNum = set()
+def d(n):
+    return n + sum(map(int, str(n)))
 
-for i in Num:
-    for j in str(i):
-        i += int(j)
-    NonSelfNum.add(i)
-    
-SelfNum = sorted(Num - NonSelfNum)
-for n in SelfNum:
-    print(n)
+NonSelfNum = set()
+for i in range(1, 10001):
+    di = d(i)
+    if di <= 10000:  # 10000을 넘지 않는 생성자에 대해서만 처리
+        NonSelfNum.add(di)
+
+SelfNum = sorted(set(range(1, 10001)) - NonSelfNum)
+print('\n'.join(map(str, SelfNum)))
