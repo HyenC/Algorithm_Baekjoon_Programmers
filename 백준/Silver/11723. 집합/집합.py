296ecd8f705a24651dd8fd233e8b382e45cd276f
HyenC <38906574+HyenC@users.noreply.github.com>
2024-02-23 03:03:39 +0900

    [Silver V] Title: 집합, Time: 5152 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+S = set()
+
+for _ in range(M):
+    order = input().split()
+    
+    if order[0] == 'add':
+        S.add(int(order[1]))
+    elif order[0] == 'remove':
+        S.discard(int(order[1]))
+    elif order[0] == 'check':
+        if int(order[1]) in S:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'toggle':
+        if int(order[1]) in S:
+            S.discard(int(order[1]))
+        else:
+            S.add(int(order[1]))
+    elif order[0] == 'all':
+        S = {i + 1 for i in range(20)}
+    else:
+        S = set()
