920967cea7a8734b101cf273d705ddcdf3283723
HyenC <38906574+HyenC@users.noreply.github.com>
2024-02-25 14:27:53 +0900

    [Gold IV] Title: 테트로미노, Time: 928 ms, Memory: 38008 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -4,7 +4,9 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
+max_value = max(map(max, board))
 answer = 0
+
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
@@ -14,14 +16,18 @@ def tetro(y, x, total, cnt):
         answer = max(answer, total)
         return
     
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
+    elif (total + max_value * (4 - cnt)) <= answer:  # 가지치기
+        return
+
+    else:
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
         
-        if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
-            visited[ny][nx] = True
-            tetro(ny, nx, total + board[ny][nx], cnt + 1)
-            visited[ny][nx] = False
+            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+                visited[ny][nx] = True
+                tetro(ny, nx, total + board[ny][nx], cnt + 1)
+                visited[ny][nx] = False
 
 # 'ㅜ' 모양 처리를 위한 함수
 def check_exc(y, x):
