249d87af50eb33ea7fd3b292cb61de8bfaeb9e0f
HyenC <38906574+HyenC@users.noreply.github.com>
2024-02-27 17:48:49 +0900

    [Silver II] Title: 좌표 압축, Time: 1408 ms, Memory: 200576 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -5,7 +5,7 @@ N = int(input())
 points = list(map(int, input().split()))
 sorted_points = sorted(set(points))
 
-dict = {point: index for index, point in enumerate(sorted_points)}
+index_dict = {point: index for index, point in enumerate(sorted_points)}
 
-for p in points:
-    print(dict[p], end=' ')
+result = ' '.join(str(index_dict[p]) for p in points)
+print(result)
