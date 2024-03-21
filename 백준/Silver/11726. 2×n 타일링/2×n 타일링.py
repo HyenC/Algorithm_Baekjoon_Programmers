cb43503a1dd80f5be9e2dfb8bec057b0acf1bd7e
HyenC <38906574+HyenC@users.noreply.github.com>
2024-03-22 01:11:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,10 +1,6 @@
-n = int(input())
+a = b = 1
 
-dp = [0] * max(n + 1, 3) 
-dp[1] = 1
-dp[2] = 2
-
-for i in range(3, n+1):
-    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+for _ in range(int(input())):
+    a, b = b, a+b
     
-print(dp[n])
+print(a % 10007)
