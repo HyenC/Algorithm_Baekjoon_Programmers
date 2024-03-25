26f65f798cbfca208868becd16383e7841ff804d
HyenC <38906574+HyenC@users.noreply.github.com>
2024-03-26 01:16:43 +0900

    [Bronze III] Title: 세 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,3 +1,16 @@
-li = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 
-print(sorted(li)[1])
+min_val = mid_val = max_val = nums[0]
+
+for num in nums[1:]:
+    if num < min_val:
+        max_val, mid_val, min_val = mid_val, min_val, num
+        
+    elif num > max_val:
+        min_val, mid_val, max_val = mid_val, max_val, num
+        
+    else:
+        # 그 외의 경우, 현재 값은 min_val과 max_val 사이
+        min_val, mid_val = min_val, num
+
+print(mid_val)
